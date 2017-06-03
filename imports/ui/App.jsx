6b7e80b63d95a39f5b3f1557f1b51dd74cea9f54
gizem <gizembg@gmail.com>
2017-06-03 19:15:29 +0300

    Step 4 - Forms and events

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-
+import ReactDOM from 'react-dom';
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
@@ -8,16 +8,26 @@ import Task from './Task.jsx';
 // App component - represents the whole app
 
 class App extends Component {
- 
+	
+   handleSubmit(event) {
+    event.preventDefault(); 
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
 
   renderTasks() {
-
     return this.props.tasks.map((task) => (
-
       <Task key={task._id} task={task} />
-
     ));
-
   }
 
   render() {
@@ -26,7 +36,14 @@ class App extends Component {
       <div className="container">
       
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List</h1>         
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
 
@@ -56,8 +73,7 @@ export default createContainer(() => {
 
   return {          
 
-    tasks: Tasks.find({}).fetch(),
-
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 
 }, App);
